0880d5ed394267570e80507c50d7360b77d81b61
Francisco Moya <francisco.moya@uclm.es>
2019-02-08 03:45:16 +0100

    Added backup into json file

---
+++ py/README.md
@@ -0,0 +1,10 @@
+# Tutorías
+
+Ejecutar el script así:
+
+```
+python tutorias.py --json --gform --html ../tutorias.html
+```
+
+De esta forma graba data.json que puede usarse para rellenar datos en el futuro.  Esto permite borrar las respuestas dadas hasta ahora en el formulario.
+
--- py/tutorias.py
+++ py/tutorias.py
@@ -7,12 +7,24 @@ def entry_to_dict(p):
     keys = ('sn','givenName','department','mail','title','telephoneNumber', 'displayName')
     return { k: str(p[k] if p[k] else '') for k in keys }
 
+def merge_into(A, B):
+    for p in B:
+        q = [i for i in A if i['mail'] == p['mail']]
+        if not q:
+            A.append(p)
+            continue
+        assert len(q) == 1
+        merge_profe(q[0], p)
+
+def merge_profe(A,B):
+    for k in B:
+        A[k] = B[k]
 
 if __name__ == '__main__':
     try: os.chdir(os.path.dirname(__file__))
     except: pass
     parser = argparse.ArgumentParser(description='Utiliza los datos de Active Directory y Formulario Google para automatizar la publicación de tutorías.')
-    parser.add_argument('--json', nargs='?', type=argparse.FileType('w'), const='data.json', default=None,
+    parser.add_argument('--json', nargs='?', type=argparse.FileType('r+'), const='data.json', default=None,
                         help='guarda los datos de profesores en archivo JSON')
     parser.add_argument('--gform', nargs='?', type=argparse.FileType('r', encoding='utf-8'), const='Solicitud cambio de horario de tutorías.csv', default=None,
                         help='recoge los datos de profesores de formulario Google')
@@ -29,11 +41,12 @@ if __name__ == '__main__':
         profes = [ entry_to_dict(p) for p in da.profesores('ESCUELA DE INGENIERÍA INDUSTRIAL TOLEDO') ]
     profes.sort(key=lambda p: (p['sn'],p['givenName']))
 
-    if args.json:
-        args.json.write(json.dumps(profes))
-    
     for p in profes:
         p['tutorias'] = 'No han sido especificadas por el profesor'
+
+    if args.json:
+        profes_prev = json.load(args.json)
+        merge_into(profes, profes_prev)
 
     if args.gform:
         gform.update(profes, args.gform)
@@ -44,3 +57,9 @@ if __name__ == '__main__':
 
     if args.js:
         args.js.write('profes={};'.format(json.dumps(profes)))
+
+    if args.json:
+        args.json.seek(0)
+        args.json.write(json.dumps(profes))
+        args.json.truncate()
+
