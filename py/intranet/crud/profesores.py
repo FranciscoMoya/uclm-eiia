956f335a6e42cd34efedce69c3b2aae42db385eb
Francisco Moya <francisco.moya@uclm.es>
2019-04-21 19:56:57 +0200

    Updated UI to admin user data

--- py/intranet/crud/profesores.py
+++ py/intranet/crud/profesores.py
@@ -6,12 +6,14 @@ JSON_DB = 'profesores.json'
 ad_keys = ('sn','givenName','department','mail','title','telephoneNumber', 'displayName')
 
 def entry_to_dict(p):
-    return { k: str(p[k] if p[k] else '') for k in ad_keys }
+    ret = { k: str(p[k] if p[k] else '') for k in ad_keys }
+    ret['userid'] = ret['mail'].split('@')[0].lower()
+    return ret
 
 class ProfesoresList(Resource):
     def get(self):
         with open(JSON_DB) as js:
-            return json.load(js)
+            return [ entry_to_dict(p) for p in json.load(js) ]
 
 class ProfesoresQuery(Resource):
     method_decorators = [auth_profesor] 
@@ -31,5 +33,10 @@ class Profesor(Resource):
 
     def get(self, userid):
         with open(JSON_DB) as js:
-            profe = [p for p in json.load(js) if p['mail'].split('@')[0].lower() == userid]
+            profe = [p for p in json.load(js) if getUserId(p) == userid]
             return profe[0] if len(profe) > 0 else ({"message": f"User {userid} not found"}, 400)
+
+def getUserId(p):
+    if 'userid' in p:
+        return p['userid']  
+    return p['mail'].split('@')[0].lower()
