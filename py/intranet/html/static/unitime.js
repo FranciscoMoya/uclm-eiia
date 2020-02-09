b2b1a79a338c73147edbd594feeab904613583f1
Francisco Moya <francisco.moya@uclm.es>
2020-02-09 11:31:35 +0100

    intranet and timetables are now separate repos in uclm-eiia

--- py/intranet/html/static/unitime.js
+++
@@ -1,5 +0,0 @@
-var e = document.evaluate( "//span/i[contains(., 'enrolled')]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
-for (var i=0 ; i < e.snapshotLength; i++){
-   e.snapshotItem(i).style.display = "none";
-}
-
--- py/intranet/start.sh
+++
@@ -1,5 +0,0 @@
-sudo mkdir -p /var/run/eii
-sudo chown nginx:nginx /var/run/eii
-sudo systemctl restart nginx
-sudo /usr/local/bin/uwsgi --ini wsgi.ini 
-
